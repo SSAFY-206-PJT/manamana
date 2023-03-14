cdb7aad19ec98579722859b2b59290ce1bbd7b01
dlaqjarb9 <dlaqjarb9@daum.net>
2023-03-14 17:35:07 +0900

    Feat : 네브바 기능 추가

--- front/.prettierrc.js
+++ front/.prettierrc.js
@@ -7,7 +7,7 @@ module.exports = {
   // 들여쓰기 너비는 2칸
   trailingComma: 'all',
   // 배열 키:값 뒤에 항상 콤마를 붙히도록 formatting
-  printWidth: 80,
+  printWidth: 100,
   // 코드 한줄이 maximum 80칸
   arrowParens: 'avoid',
   // 화살표 함수가 하나의 매개변수를 받을 때 괄호를 생략하게 formatting
---
+++ front/pages/search/index.tsx
@@ -0,0 +1,10 @@
+import Navbar from '../../components/Navbar';
+
+export default function SearchPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Search Page</h1>
+      <Navbar />
+    </div>
+  );
+}
