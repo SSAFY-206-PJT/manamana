8d798539d98c1dbdb4d6d9c40348a44f9cb0e71e
lsh80165 <lsh80165@gmail.com>
2023-03-14 17:28:38 +0900

    Feat : 커스텀 예외 등록, 커스텀 예외 처리를 위한 controllerAdvisor 등록

---
+++ manamana/src/main/java/com/webtoon/manamana/config/advisor/ControllerAdvisor.java
@@ -0,0 +1,52 @@
+package com.webtoon.manamana.config.advisor;
+
+import com.webtoon.manamana.config.response.CommonResponse;
+import com.webtoon.manamana.config.response.ResponseService;
+import com.webtoon.manamana.config.response.exception.CustomException;
+import com.webtoon.manamana.config.response.exception.CustomExceptionStatus;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.repository.cdi.Eager;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
+@Getter
+@RequiredArgsConstructor
+@RestControllerAdvice
+public class ControllerAdvisor {
+
+    //에러나면 ResponseService에서 에러난 것을 던져주기 위해
+    private final ResponseService responseService;
+
+
+    //커스텀 예외 처리 - 커스텀 예외를 먼저 처리하도록 둠.
+    public CommonResponse exceptionHandler(CustomException e){
+
+        CustomExceptionStatus status = e.getCustomExceptionStatus();
+
+        log.warn("[" +" CustomException - " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")) + "]" +" : " + status.getMessage());
+
+        return responseService.getExceptionResponse(status);
+
+    }
+
+
+    //커스텀 예외로 확인되지 않은 모든 런타임 예외
+    public CommonResponse exceptionHandler(RuntimeException e){
+
+
+        log.error("[" +" CustomException - " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")) + "]" +" : " + e.getMessage());
+
+        CommonResponse response = new CommonResponse();
+        response.setSuccess(false);
+        response.setCode(400);
+        response.setMessage(e.getMessage());
+
+        return response;
+    }
+
+}
