882156e3a05b8c344250ada12c8775eb3bde6353
김주하 <zouamare@gmail.com>
2023-03-15 17:03:02 +0900

    Feat: 로그인 페이지 UI 구현

--- front/components/pages/login/MovingLogo.tsx
+++ front/components/pages/login/MovingLogo.tsx
@@ -1,84 +1,151 @@
 import Image from 'next/image'
+import { css } from "@emotion/react";
+import React, { useRef } from "react";
 
-export default function MovingLogo() {
+export default function MovingLogo(props: {
+    direction: string;
+    startIdx: number;
+}) {
 
-    //  https://blogpack.tistory.com/1120
-    // //인터벌 메서드로 애니메이션 생성
-    // let rollerWidth = document.querySelector('.roller ul').offsetWidth;//회전 배너 너비값
-    // let betweenDistance = 1;//이동 크기 - 정수여야 함
-    // originalID = window.setInterval(betweenRollCallback, parseInt(1000 / 100), betweenDistance, document.querySelector('#roller1'));
-    // cloneID = window.setInterval(betweenRollCallback, parseInt(1000 / 100), betweenDistance, document.querySelector('#roller2'));
+    /*
+    *   @variable
+    *   로고 이미지 변수로 지정
+    */
+    const lezhinLogo = <Image
+        src="/images/Lezhin_Comics_Logo.png"
+        alt={'lezhin comics'}
+        width={'150'}
+        height={'150'}
+        className='inline-block rounded-3xl'
+    />;
 
-    // //인터벌 애니메이션 함수(공용)
-    // function betweenRollCallback(d, roller) {
-    //     let left = parseInt(roller.style.left);
-    //     roller.style.left = (left - d) + 'px';//이동
-    //     //조건부 위치 리셋
-    //     if (rollerWidth + (left - d) <= 0) {
-    //         roller.style.left = rollerWidth + 'px';
-    //     }
-    // }
+    const naverLogo = <Image
+        src="/images/Naver_Webtoon_Logo.svg"
+        alt={'naver webtoon'}
+        width={'150'}
+        height={'150'}
+        className='inline-block rounded-3xl'
+    />;
 
-    return (<div>
-        <div className='container overflow-visible flex flex-row -translate-x-2 transition-transform duration-100'>
-            <Image
-                src="/images/Lezhin_Comics_Logo.png"
-                alt={'lezhin comics'}
-                width={'100'}
-                height={'100'}
-                className='inline-block rounded-3xl'
-            />
-            <Image
-                src="/images/Naver_Webtoon_Logo.png"
-                alt={'naver webtoon'}
-                width={'100'}
-                height={'100'}
-                className='inline-block rounded-3xl'
-            />
-            <Image
-                src="/images/Kakao_Page_Logo.png"
-                alt={'kakao page'}
-                width={'100'}
-                height={'100'}
-                className='inline-block rounded-3xl'
-            />
-            <Image
-                src="/images/Kakao_Webtoon_Logo.png"
-                alt={'kakao webtoon'}
-                width={'100'}
-                height={'100'}
-                className='inline-block rounded-3xl'
-            />
+    const kakaoPageLogo = <Image
+        src="/images/Kakao_Page_Logo.png"
+        alt={'kakao page'}
+        width={'150'}
+        height={'150'}
+        className='inline-block rounded-3xl'
+    />;
+
+    const kakaoWebtoonLogo = <Image
+        src="/images/Kakao_Webtoon_Logo.png"
+        alt={'kakao webtoon'}
+        width={'150'}
+        height={'150'}
+        className='inline-block rounded-3xl'
+    />;
+
+    /*
+    *   @variable
+    *   로고 이미지 변수 배열에 저장
+    */
+    const LogoType = [lezhinLogo, naverLogo, kakaoPageLogo, kakaoWebtoonLogo];
+
+    /*
+    *   @variable
+    *   로고 이미지를 저장할 배열
+    */
+    let LogoList = [];
+
+    /*
+    *   @loop
+    *   로고 이미지를 저장하기 위한 반복문
+    */
+    for (let i = 0; i < 40; i++) {
+        LogoList.push(LogoType[(props.startIdx + i) % LogoType.length]);
+    }
+
+
+    /*
+    *   @Method
+    *   왼쪽으로 움직이는 코드
+    */
+    let moveLeft = () => {
+        return (css`
+        animation: 50s linear 1s infinite normal none running moveLeft;
+
+        @keyframes moveLeft {
+            from {
+                transform: translateX(0);
+            }
+            to {
+                transform: translateX(-100%);
+            }
+        }
+    `);
+    }
+
+    let position = 0;
+    const containerRef = useRef<HTMLDivElement>(null);
+    const logoRef = useRef<HTMLDivElement>(null);
+
+    /*
+    *   @Method
+    *   오른쪽으로 움직이는 코드
+    */
+    let moveRight = () => {
+        if (logoRef.current != null && containerRef.current != null) {
+            position += 5;
+            logoRef.current.style.left = `${position}px`;
+
+            if (position >= containerRef.current.offsetWidth) {
+                position = 0;
+
+            }
+
+            setTimeout(moveRight, 10);
+        }
+
+        console.dir(logoRef.current);
+
+        return (
+            css``
+            //         `
+            //     animation: 50s linear 1s infinite normal none running moveRight;
+
+            //     @keyframes moveRight {
+            //         to {
+            //             transform: translateX(100%);
+            //         }
+            //     }
+            // `
+        );
+    }
+
+
+
+
+    return (<div
+        className='w-full overflow-hidden h-fit'
+        ref={containerRef}
+        css={
+            css`
+            `
+        }
+    >
+        <div
+            ref={logoRef}
+            css={
+                props.direction == "left" ? moveLeft() : moveRight()
+            }
+            className='float-left flex'>
+            {LogoList}
         </div>
-        {/* <div className='container overflow-visible flex flex-row translate-x-2'>
-            <Image
-                src="/images/Lezhin_Comics_Logo.png"
-                alt={'lezhin comics'}
-                width={'100'}
-                height={'100'}
-                className='inline-block rounded-3xl'
-            />
-            <Image
-                src="/images/Naver_Webtoon_Logo.png"
-                alt={'naver webtoon'}
-                width={'100'}
-                height={'100'}
-                className='inline-block rounded-3xl'
-            />
-            <Image
-                src="/images/Kakao_Page_Logo.png"
-                alt={'kakao page'}
-                width={'100'}
-                height={'100'}
-                className='inline-block rounded-3xl'
-            />
-            <Image
-                src="/images/Kakao_Webtoon_Logo.png"
-                alt={'kakao webtoon'}
-                width={'100'}
-                height={'100'}
-                className='inline-block rounded-3xl'
-            />
+        {/* <div
+            ref={logoRef}
+            css={
+                props.direction == "left" ? moveLeft() : moveRight()
+            }
+            className='float-left flex'>
+            {LogoList}
         </div> */}
     </div>);
 }
--- front/next.config.js
+++ front/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: config => {
+    // 아래를 추가합니다.
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"]
+    });
+    return config;
+  }
 }
 
 module.exports = nextConfig
