79962a8d153c0d9c0bd6ff65141cf4acafbf4127
lsh80165 <lsh80165@gmail.com>
2023-03-15 16:13:18 +0900

    Feat : querydsl 동작 테스트

--- manamana/src/main/java/com/webtoon/manamana/ManamanaApplication.java
+++ manamana/src/main/java/com/webtoon/manamana/ManamanaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class ManamanaApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ManamanaApplication.class, args);
 	}
