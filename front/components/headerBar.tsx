f441fa007ae66661ec47a99165a1e45a5694b4e7
dlaqjarb9 <dlaqjarb9@daum.net>
2023-03-15 10:49:42 +0900

    Feat : 헤더 컴포넌트 추가

--- front/components/headerBar.tsx
+++ front/components/headerBar.tsx
@@ -1,17 +1,10 @@
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 
-/**
- *
- * @param {Boolean} showBackBtn 뒤로가기 버튼 유무
- * @param {string} pageName 페이지 이름
- * @param {Boolean} showNotiBtn 알림 버튼 유무
- * @returns
- */
 type Props = {
   showBackBtn: Boolean;
-  pageName: string;
-  showNotiBtn: Boolean;
+  pageName?: string;
+  rightBtn?: 'EDIT' | 'NOTI' | undefined;
 };
 
 export default function Headerbar(props: Props) {
@@ -19,27 +12,48 @@ export default function Headerbar(props: Props) {
 
   let showBackBtn = props.showBackBtn;
   let pageName = props.pageName;
-  let showNotiBtn = props.showNotiBtn;
+  let rightBtnOpt = props.rightBtn;
 
-  // 뒤로가기 버튼
-  let backBtn = showBackBtn ? (
-    <div className="">
-      <img src="/images/HeaderBar_Back.png" alt="goBack" onClick={() => router.back()}></img>
-    </div>
-  ) : (
-    <div className="w-6 h-6"></div>
-  );
+  // 왼쪽 버튼 (뒤로가기)
+  let backBtn =
+    // 왼쪽 : 뒤로가기, 오른쪽 : 편집 버튼인 경우
+    showBackBtn && rightBtnOpt === 'EDIT' ? (
+      <div className="w-12 h-6">
+        <img src="/images/HeaderBar_Back.png" alt="goBack" onClick={() => router.back()}></img>
+      </div>
+    ) : // 왼쪽 : 뒤로가기, 오른쪽 : 편집 버튼이 아닌 경우
+    showBackBtn ? (
+      <div className="">
+        <img src="/images/HeaderBar_Back.png" alt="goBack" onClick={() => router.back()}></img>
+      </div>
+    ) : (
+      // 왼쪽에 뒤로가기가 없는 경우
+      <div className="w-6 h-6"></div>
+    );
 
-  // 알림 버튼
-  let notiBtn = showNotiBtn ? (
-    <div>
-      <Link href="/notification">
-        <img src="/images/HeaderBar_Noti.png" alt="notification"></img>
-      </Link>
-    </div>
-  ) : (
-    <div className="w-6 h-6"></div>
-  );
+  // 오른쪽 버튼 (알림, 편집, null)
+  let showRightBtn = null;
+  switch (rightBtnOpt) {
+    case 'NOTI': // 알림
+      showRightBtn = (
+        <div>
+          <Link href="/notification">
+            <img src="/images/HeaderBar_Noti.png" alt="notification"></img>
+          </Link>
+        </div>
+      );
+      break;
+    case 'EDIT': // 편집
+      showRightBtn = (
+        <div className="flex justify-center items-center w-12 h-6 text-sm border-2 rounded-md border-BackgroundLightComponentBolder">
+          편집
+        </div>
+      );
+      break;
+    default:
+      showRightBtn = <div className="w-6 h-6"></div>;
+      break;
+  }
 
   return (
     <div className="flex justify-between items-center p-5 h-14 bg-BackgroundLightComponent">
@@ -47,7 +61,7 @@ export default function Headerbar(props: Props) {
       <div className="">
         <p className="font-bold text-xl">{pageName}</p>
       </div>
-      {notiBtn}
+      {showRightBtn}
     </div>
   );
 }
--- front/pages/search/index.tsx
+++ front/pages/search/index.tsx
@@ -1,10 +1,10 @@
 import Navbar from '../../components/navBar';
-import Headerbar from '../../components/headerBar'
+import Headerbar from '../../components/headerBar';
 
 export default function SearchPage() {
   return (
     <div>
-      <Headerbar showBackBtn={false} pageName="탐색" showNotiBtn={true} />
+      <Headerbar showBackBtn={true} pageName="내 웹툰" rightBtn="EDIT" />
       <h1 className="text-3xl font-bold underline">Search Page</h1>
       <Navbar />
     </div>
