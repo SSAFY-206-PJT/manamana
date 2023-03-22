b11355ac88be5728d8c58f49bac8bc744d56c83f
lsh80165 <lsh80165@gmail.com>
2023-03-22 14:54:23 +0900

    Feat : 테스트 api - 취향 월드컵에 필요한 웹툰 조회 완성

--- backend/manamana/src/main/java/com/webtoon/manamana/recommand/controller/RecommandController.java
+++ backend/manamana/src/main/java/com/webtoon/manamana/recommand/controller/RecommandController.java
@@ -106,13 +106,54 @@ public class RecommandController {
         return responseService.getDataResponse(jsonArray, CustomSuccessStatus.RESPONSE_SUCCESS);
     }
 //
-//    /*취향 월드컵 조회*/
-//    @GetMapping("/world-cup")
-//    public DataResponse<Object> worldCupWebtoon(){
-//
-//
-//        return responseService.getDataResponse(, CustomSuccessStatus.RESPONSE_SUCCESS);
-//    }
+    /*취향 월드컵 조회*/
+    @GetMapping("/world-cup")
+    public DataResponse<Object> worldCupWebtoon() throws Exception{
+
+        String temp1 = "{\n" +
+                "\t\t\t\"id\": 1, \n" +
+                "\t\t\t\"imagePath\": \"https://image-comic.pstatic.net/webtoon/774044/thumbnail/thumbnail_IMAG21_81504afb-1a05-41b0-9650-0c9aa1d741d9.jpg\"\n" +
+                "\t\t}";
+        String temp2 = "{\n" +
+                "\t\t\t\"id\": 2, \n" +
+                "\t\t\t\"imagePath\": \"https://image-comic.pstatic.net/webtoon/805691/thumbnail/thumbnail_IMAG21_8b092acf-e28c-47d2-9b5f-3a02df85103f.jpg\"\n" +
+                "\t\t}";
+        String temp3 = "{\n" +
+                "\t\t\t\"id\": 3, \n" +
+                "\t\t\t\"imagePath\": \"https://image-comic.pstatic.net/webtoon/795529/thumbnail/thumbnail_IMAG21_3689631394330325350.jpg\"\n" +
+                "\t\t}";
+        String temp4 = "{\n" +
+                "\t\t\t\"id\": 4, \n" +
+                "\t\t\t\"imagePath\": \"https://image-comic.pstatic.net/webtoon/736277/thumbnail/thumbnail_IMAG21_bbbe3f76-021e-4dbc-830a-4358c1abec0c.jpg\"\n" +
+                "\t\t}";
+        String temp5 = "{\n" +
+                "\t\t\t\"id\": 5, \n" +
+                "\t\t\t\"imagePath\": \"https://image-comic.pstatic.net/webtoon/759567/thumbnail/thumbnail_IMAG21_3991657355780766050.jpg\"\n" +
+                "\t\t}";
+        String temp6 = "{\n" +
+                "\t\t\t\"id\": 6, \n" +
+                "\t\t\t\"imagePath\": \"https://image-comic.pstatic.net/webtoon/695796/thumbnail/thumbnail_IMAG21_332bb25b-c77d-477f-9979-5a8607ebd7a5.jpg\"\n" +
+                "\t\t}";
+
+        JSONArray jsonArray = new JSONArray();
+        JSONParser jsonParser = new JSONParser();
+
+        JSONObject jsonObj1 = (JSONObject) jsonParser.parse(temp1);
+        JSONObject jsonObj2 = (JSONObject) jsonParser.parse(temp2);
+        JSONObject jsonObj3 = (JSONObject) jsonParser.parse(temp3);
+        JSONObject jsonObj4 = (JSONObject) jsonParser.parse(temp4);
+        JSONObject jsonObj5 = (JSONObject) jsonParser.parse(temp5);
+        JSONObject jsonObj6 = (JSONObject) jsonParser.parse(temp6);
+
+        jsonArray.add(jsonObj1);
+        jsonArray.add(jsonObj2);
+        jsonArray.add(jsonObj3);
+        jsonArray.add(jsonObj4);
+        jsonArray.add(jsonObj5);
+        jsonArray.add(jsonObj6);
+
+        return responseService.getDataResponse(jsonArray, CustomSuccessStatus.RESPONSE_SUCCESS);
+    }
 //
 //    /*취향 월드컵 결과 저장.*/
 //    @PostMapping("/world-cup")
