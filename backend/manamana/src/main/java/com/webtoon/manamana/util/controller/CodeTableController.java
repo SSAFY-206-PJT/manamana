e48273cba896ad34d6466e84ae717ea8edfb9a82
lsh80165 <lsh80165@gmail.com>
2023-03-22 13:44:11 +0900

    Feat : 테스트 api - top10 키워드 목록 조회 완성

--- backend/manamana/src/main/java/com/webtoon/manamana/util/controller/CodeTableController.java
+++ backend/manamana/src/main/java/com/webtoon/manamana/util/controller/CodeTableController.java
@@ -26,7 +26,7 @@ public class CodeTableController {
 
     /*연재 여부 목록*/
     @GetMapping("/status")
-    public DataResponse<Object> stautsList() throws Exception{
+    public DataResponse<Object> statusList() throws Exception{
 
         String temp1 = "{\n" +
                 "\t\t\t\t\t\"id\": 1,\n" +
@@ -205,13 +205,77 @@ public class CodeTableController {
 
         return responseService.getDataResponse(jsonArray, CustomSuccessStatus.RESPONSE_SUCCESS);
     }
-//    /*웹툰 키워드 top10*/
-//    @GetMapping("keywords")
-//    public DataResponse<Object> stautsList() throws Exception{
-//
-//
-//        return responseService.getDataResponse(, CustomSuccessStatus.RESPONSE_SUCCESS);
-//    }
+    /*웹툰 키워드 top10*/
+    @GetMapping("/keywords")
+    public DataResponse<Object> top10List() throws Exception{
+        String temp1 = "{\n" +
+                "\t\t\t\t\t\"rank\": 1,\n" +
+                "\t\t\t\t\t\"keyword\": \"1초\"\n" +
+                "\t\t\t}";
+        String temp2 = "{\n" +
+                "\t\t\t\t\t\"rank\": 2,\n" +
+                "\t\t\t\t\t\"keyword\": \"투신 전생기\"\n" +
+                "\t\t\t}";
+        String temp3 = "{\n" +
+                "\t\t\t\t\t\"rank\": 3,\n" +
+                "\t\t\t\t\t\"keyword\": \"나 혼자 레벨업\"\n" +
+                "\t\t\t}";
+        String temp4 = "{\n" +
+                "\t\t\t\t\t\"rank\": 4,\n" +
+                "\t\t\t\t\t\"keyword\": \"대학원 탈출일지\"\n" +
+                "\t\t\t}";
+        String temp5 = "{\n" +
+                "\t\t\t\t\t\"rank\": 5,\n" +
+                "\t\t\t\t\t\"keyword\": \"호랑이 형님\"\n" +
+                "\t\t\t}";
+        String temp6 = "{\n" +
+                "\t\t\t\t\t\"rank\": 6,\n" +
+                "\t\t\t\t\t\"keyword\": \"검은 인간\"\n" +
+                "\t\t\t}";
+        String temp7 = "{\n" +
+                "\t\t\t\t\t\"rank\": 7,\n" +
+                "\t\t\t\t\t\"keyword\": \"행성인간\"\n" +
+                "\t\t\t}";
+        String temp8 = "{\n" +
+                "\t\t\t\t\t\"rank\": 8,\n" +
+                "\t\t\t\t\t\"keyword\": \"데드퀸\"\n" +
+                "\t\t\t}";
+        String temp9 = "{\n" +
+                "\t\t\t\t\t\"rank\": 9,\n" +
+                "\t\t\t\t\t\"keyword\": \"일렉시드\"\n" +
+                "\t\t\t}";
+        String temp10 = "{\n" +
+                "\t\t\t\t\t\"rank\": 10,\n" +
+                "\t\t\t\t\t\"keyword\": \"나혼자 만렙 뉴비\"\n" +
+                "\t\t\t}";
+
+        JSONArray jsonArray = new JSONArray();
+        JSONParser jsonParser = new JSONParser();
+
+        JSONObject jsonObj1 = (JSONObject) jsonParser.parse(temp1);
+        JSONObject jsonObj2 = (JSONObject) jsonParser.parse(temp2);
+        JSONObject jsonObj3 = (JSONObject) jsonParser.parse(temp3);
+        JSONObject jsonObj4 = (JSONObject) jsonParser.parse(temp4);
+        JSONObject jsonObj5 = (JSONObject) jsonParser.parse(temp5);
+        JSONObject jsonObj6 = (JSONObject) jsonParser.parse(temp6);
+        JSONObject jsonObj7 = (JSONObject) jsonParser.parse(temp7);
+        JSONObject jsonObj8 = (JSONObject) jsonParser.parse(temp8);
+        JSONObject jsonObj9 = (JSONObject) jsonParser.parse(temp9);
+        JSONObject jsonObj10 = (JSONObject) jsonParser.parse(temp10);
+
+        jsonArray.add(jsonObj1);
+        jsonArray.add(jsonObj2);
+        jsonArray.add(jsonObj3);
+        jsonArray.add(jsonObj4);
+        jsonArray.add(jsonObj5);
+        jsonArray.add(jsonObj6);
+        jsonArray.add(jsonObj7);
+        jsonArray.add(jsonObj8);
+        jsonArray.add(jsonObj9);
+        jsonArray.add(jsonObj10);
+
+        return responseService.getDataResponse(jsonArray, CustomSuccessStatus.RESPONSE_SUCCESS);
+    }
 //
 //    /*웹툰 장르 목록*/
 //    @GetMapping("/genres")public DataResponse<Object> stautsList() throws Exception{
