2f7b669479059ec7406ab57797c39c99b633d574
happyd918 <0happydeath0@naver.com>
2023-03-23 11:10:18 +0900

    Feat : my-comment

--- frontend/components/pages/comment/ChatListItem.tsx
+++ frontend/components/pages/comment/ChatListItem.tsx
@@ -38,28 +38,136 @@ function ChatListItem({ chat }: ChatProp) {
   };
 
   const [commentModal, setCommentModal] = useState<any>(false);
+  const [modalState, setModalState] = useState<string>('init');
 
   const openModal = () => {
     setCommentModal(true);
   };
   const closeModal = () => {
     setCommentModal(false);
+    setModalState('init');
+  };
+
+  const changeModalState = (e: string) => {
+    setModalState(e);
+  };
+  const deleteChat = () => {
+    alert('삭제');
+  };
+  const modifyChat = () => {
+    alert('수정');
+  };
+  const reportChat = () => {
+    alert('신고');
+  };
+
+  const popupDelete = (
+    <div className="flex justify-center">
+      <div className="my-6 flex w-2/3 flex-col items-center justify-center">
+        <p className="text-center text-2xl text-PrimaryLight">이 댓글을 정말 삭제하시겠습니까?</p>
+        <hr className="my-2 w-full border border-PrimaryLight bg-PrimaryLight" />
+        <button
+          className="flex w-1/2 flex-col items-center justify-center py-3"
+          onClick={deleteChat}
+        >
+          <p className="text-center">삭제하기</p>
+        </button>
+      </div>
+    </div>
+  );
+
+  const popupModify = (
+    <div className="flex justify-center">
+      <div className="my-6 flex w-2/3 flex-col items-center justify-center">
+        <p className="text-center text-2xl text-PrimaryLight">이 댓글을 정말 수정하시겠습니까?</p>
+        <hr className="my-2 w-full border border-PrimaryLight bg-PrimaryLight" />
+        <button
+          className="flex w-1/2 flex-col items-center justify-center py-3"
+          onClick={modifyChat}
+        >
+          <p className="text-center">수정하기 div</p>
+        </button>
+      </div>
+    </div>
+  );
+
+  const popupReport = (
+    <div className="flex justify-center">
+      <div className="my-6 flex w-2/3 flex-col items-center justify-center">
+        <p className="text-center text-2xl text-PrimaryLight">이 댓글을 정말 신고하시겠습니까?</p>
+        <hr className="my-2 w-full border border-PrimaryLight bg-PrimaryLight" />
+        <button
+          className="flex w-1/2 flex-col items-center justify-center py-3"
+          onClick={reportChat}
+        >
+          <p className="text-center">신고하기</p>
+        </button>
+      </div>
+    </div>
+  );
+
+  const popupListForUser = (
+    <div className="flex justify-center">
+      <div className="my-6 flex w-2/3 flex-col items-center justify-center">
+        <p className="text-center text-2xl text-PrimaryLight">댓글 설정</p>
+        <hr className="my-2 w-full border border-PrimaryLight bg-PrimaryLight" />
+        <button
+          className="flex w-1/2 flex-col items-center justify-center py-3"
+          onClick={() => {
+            changeModalState('del');
+          }}
+        >
+          <p className="text-center">삭제하기</p>
+        </button>
+        <button
+          className="flex w-1/2 flex-col items-center justify-center py-3"
+          onClick={() => {
+            changeModalState('modify');
+          }}
+        >
+          <p className="text-center">수정하기</p>
+        </button>
+      </div>
+    </div>
+  );
+
+  const popupListForElse = (
+    <div className="flex justify-center">
+      <div className="my-6 flex w-2/3 flex-col items-center justify-center">
+        <p className="text-center text-2xl text-PrimaryLight">댓글 설정</p>
+        <hr className="my-2 w-full border border-PrimaryLight bg-PrimaryLight" />
+        <button
+          className="flex w-1/2 flex-col items-center justify-center py-3"
+          onClick={() => {
+            changeModalState('report');
+          }}
+        >
+          <p className="text-center">신고하기</p>
+        </button>
+      </div>
+    </div>
+  );
+
+  const popupForUser = () => {
+    if (modalState === 'init') {
+      return popupListForUser;
+    } else if (modalState === 'del') {
+      return popupDelete;
+    } else if (modalState === 'modify') {
+      return popupModify;
+    }
+  };
+  const popupForElse = () => {
+    if (modalState === 'init') {
+      return popupListForElse;
+    } else if (modalState === 'report') {
+      return popupReport;
+    }
   };
 
   const popupDiv = (
     <SwipeableDrawer anchor={'bottom'} open={commentModal} onOpen={openModal} onClose={closeModal}>
-      <div className="flex justify-center">
-        <div className="my-6 flex w-2/3 flex-col items-center justify-center">
-          <p className="text-center text-2xl text-PrimaryLight">댓글 설정</p>
-          <hr className="my-2 w-full border border-PrimaryLight bg-PrimaryLight" />
-          <button className="my-3 flex flex-col items-center justify-center">
-            <p className="text-center">삭제하기</p>
-          </button>
-          <button className="my-3 flex flex-col items-center justify-center">
-            <p className="text-center">신고하기</p>
-          </button>
-        </div>
-      </div>
+      {chat.user.nickname === myName ? popupForUser() : popupForElse()}
     </SwipeableDrawer>
   );
 
--- frontend/pages/my-comment/index.tsx
+++ frontend/pages/my-comment/index.tsx
@@ -1,3 +1,14 @@
+import ChatList from '@/components/pages/comment/ChatList';
+import Headerbar from '@/components/common/Headerbar';
+
 export default function MyCommentPage() {
-  return <h1 className="text-3xl font-bold underline">My-Comment Page</h1>;
+  return (
+    <div>
+      <div className="flex h-screen w-full flex-col">
+        <Headerbar showBackBtn={true} pageName={''} rightBtn={'NOTI'} />
+        <ChatList />
+        <div className="m-1.5"></div>
+      </div>
+    </div>
+  );
 }
