97b0e72066a84377eea0a96a231535b1eb8d1d99
yyh0126 <yyh0126@naver.com>
2023-03-23 14:05:00 +0900

    Fix : tqdm, print 제거

--- crawling/kakao_page/kakaoPageCrawling.py
+++ crawling/kakao_page/kakaoPageCrawling.py
@@ -22,10 +22,11 @@ error_list = []
 """
 요일별 크롤링 시작
 """
-# day_id = ["1", "2", "3", "4", "5", "6", "7", "12"]
-day_id = ["1"]
+day_id = ["1", "2", "3", "4", "5", "6", "7", "12"]
+# day_id = ["1"]
 
-for d_id in tqdm(day_id):  # 배포할때 tqdm 빼야함
+# for d_id in tqdm(day_id):  # 배포할때 tqdm 빼야함
+for d_id in day_id:
 
     url = "https://page.kakao.com/menu/10/screen/5?tab_uid="
     driver.get(url + d_id)
@@ -72,9 +73,6 @@ for d_id in tqdm(day_id):  # 배포할때 tqdm 빼야함
         total_url.append(temp)
         i += 1
 
-    # print(len(total_url))
-#     print(total_url)
-
     # 크롬 메모리초과 방지
     ##########################
     driver.quit()
@@ -88,7 +86,8 @@ for d_id in tqdm(day_id):  # 배포할때 tqdm 빼야함
     """
     웹툰 정보 크롤링
     """
-    for webtoon_id in tqdm(total_url):  # 배포할때 tqdm 빼야됨
+    # for webtoon_id in tqdm(total_url):  # 배포할때 tqdm 빼야됨
+    for webtoon_id in total_url:
 
         try:
 
@@ -99,8 +98,6 @@ for d_id in tqdm(day_id):  # 배포할때 tqdm 빼야함
 
             webtoon_info.webtoon_id = webtoon_id
             webtoon_info.webtoon_url = webtoon_url
-            #         print(webtoon_id)
-            #         print(webtoon_url)
 
             driver.get(webtoon_url)
             time.sleep(2)
@@ -110,31 +107,24 @@ for d_id in tqdm(day_id):  # 배포할때 tqdm 빼야함
 
             webtoon_info.name = soup.select_one(
                 '#__next > div > div.flex.w-full.grow.flex-col.px-122pxr > div.flex.h-full.flex-1 > div.mb-28pxr.flex.w-320pxr.flex-col > div:nth-child(1) > div.w-320pxr.css-0 > div > div.css-0 > div.relative.text-center.mx-32pxr.py-24pxr > span').get_text()
-            #         print(title)
 
             authors = soup.select_one('#__next > div > div.flex.w-full.grow.flex-col.px-122pxr > div.flex.h-full.flex-1 > div.mb-28pxr.flex.w-320pxr.flex-col > div:nth-child(1) > div.w-320pxr.css-0 > div > div.css-0 > div.relative.text-center.mx-32pxr.py-24pxr > div.flex.items-center.justify-center.mt-4pxr.flex-col.text-el-50.opacity-100.all-child\:font-small2 > div.mt-4pxr > span').get_text()
             webtoon_info.authors_arr = re.split(',\s|,', authors)
-            #         print(authors)
 
             day_status = soup.select_one('#__next > div > div.flex.w-full.grow.flex-col.px-122pxr > div.flex.h-full.flex-1 > div.mb-28pxr.flex.w-320pxr.flex-col > div:nth-child(1) > div.w-320pxr.css-0 > div > div.css-0 > div.relative.text-center.mx-32pxr.py-24pxr > div.flex.items-center.justify-center.mt-4pxr.flex-col.text-el-50.opacity-100.all-child\:font-small2 > div:nth-child(1) > span').get_text()
             day_status = re.split(',\s|\s', day_status)
             webtoon_info.day_arr = day_status[:-1]
             webtoon_info.status = day_status[-1]
-            #         print(day)
-            #         print(status)
 
             image = str(soup.select('#__next > div > div.flex.w-full.grow.flex-col.px-122pxr > div.flex.h-full.flex-1 > div.mb-28pxr.flex.w-320pxr.flex-col > div:nth-child(1) > div.w-320pxr.css-0 > div > div.css-0 > div.mx-auto.css-1cyn2un-ContentOverviewThumbnail > div > div > img'))
             image = image[image.find("src")+5:-4]
             webtoon_info.image = image
-            #         print(image)
 
             total_ep = soup.select_one('#__next > div > div.flex.w-full.grow.flex-col.px-122pxr > div.flex.h-full.flex-1 > div.mb-28pxr.ml-4px.flex.w-632pxr.flex-col > div:nth-child(2) > div:nth-child(1) > div.flex.h-44pxr.w-full.flex-row.items-center.justify-between.px-15pxr.bg-bg-a-20 > div.flex.h-full.flex-1.items-center.space-x-8pxr > span').get_text()
             webtoon_info.total_ep = total_ep[3:]
-            #         print(total_episode)
 
             webtoon_info.start_date = soup.select_one(
                 '#__next > div > div.flex.w-full.grow.flex-col.px-122pxr > div.flex.h-full.flex-1 > div.mb-28pxr.ml-4px.flex.w-632pxr.flex-col > div:nth-child(2) > div:nth-child(1) > div.min-h-360pxr > ul > li:nth-child(1) > div > div > a > div > div.flex.flex-col > div.text-ellipsis.line-clamp-1.font-small2.text-el-60 > span').get_text()
-            #         print(start_date)
 
             # 작품소개로 이동
             info_url = webtoon_url + "?tab_type=about"
@@ -153,7 +143,6 @@ for d_id in tqdm(day_id):  # 배포할때 tqdm 빼야함
 
             webtoon_info.plot = soup.select_one(
                 '#__next > div > div.flex.w-full.grow.flex-col.px-122pxr > div.flex.h-full.flex-1 > div.mb-28pxr.ml-4px.flex.w-632pxr.flex-col > div.flex-1.bg-bg-a-20 > div.text-el-60.break-keep.py-20pxr.pt-31pxr.pb-32pxr > span').get_text()
-            #         print(plot)
 
             grade = soup.select_one(
                 '#__next > div > div.flex.w-full.grow.flex-col.px-122pxr > div.flex.h-full.flex-1 > div.mb-28pxr.ml-4px.flex.w-632pxr.flex-col > div.flex-1.bg-bg-a-20 > div.flex.pr-32pxr > div:nth-child(1) > div.mt-16pxr.px-32pxr > div:nth-child(3) > div').get_text()
@@ -162,11 +151,9 @@ for d_id in tqdm(day_id):  # 배포할때 tqdm 빼야함
             if (grade == "19세이용가") or ("19" in grade):
                 grade = "성인"
             webtoon_info.grade = grade
-            #         print(grade)
 
             hsl = image_main_color_hsl(image)
             webtoon_info.colorHsl = hsl
-            #         print(hsl)
 
             webtoon_info.done()
 
