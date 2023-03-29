9902da7704d40010b0c15380e86f4341ebae7274
happyd918 <0happydeath0@naver.com>
2023-03-29 14:00:29 +0900

    Feat : pwa

--- frontend/pages/profile/index.tsx
+++ frontend/pages/profile/index.tsx
@@ -137,7 +137,7 @@ export default function ProfilePage({ userData }: any) {
                 <input
                   className="w-full border-[1px] text-xl font-medium text-FontPrimaryLight"
                   type="text"
-                  maxLength="8"
+                  maxLength={8}
                   value={info.nickname}
                   onChange={changeNickname}
                 ></input>
@@ -196,7 +196,7 @@ export default function ProfilePage({ userData }: any) {
 
 export const getServerSideProps: GetServerSideProps = async context => {
   // const { user_id } = context.query;
-  const user_id = 1; // 로그인 구현 전이라 임시로 user_id 설정
+  const user_id = 3; // 로그인 구현 전이라 임시로 user_id 설정
   try {
     const response = await axios.get(`https://j8b206.p.ssafy.io/api/users/${user_id}`);
     const userData: User = response.data.result;
