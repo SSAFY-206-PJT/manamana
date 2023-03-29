2a4244ac3d12aee8ff51699812a25867d0decd4d
lsh80165 <lsh80165@gmail.com>
2023-03-29 14:51:50 +0900

    Fix : 긴급 수정

--- backend/manamana/src/main/java/com/webtoon/manamana/config/WebConfig.java
+++ backend/manamana/src/main/java/com/webtoon/manamana/config/WebConfig.java
@@ -32,14 +32,14 @@ public class WebConfig implements WebMvcConfigurer {
 
 //    // TODO : 추후에 지워야됨, 또한 시큐리티 사용시 바꿔야됨.
 //    /*CORS 처리*/
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry
-//                .addMapping("/**")
-//                .allowedOrigins("*") //외부에서 들어오는 모든 url 허용
-//                .allowedMethods("GET","POST","PATCH","DELETE") //허용되는 메서드
-//                .allowedHeaders("*")//허용되는 헤더 지원
-//                .allowCredentials(true) //자격증명 허용
-//                .maxAge(MAX_AGE_SECS);
-//    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("*") //외부에서 들어오는 모든 url 허용
+                .allowedMethods("GET","POST","PATCH","DELETE") //허용되는 메서드
+                .allowedHeaders("*")//허용되는 헤더 지원
+                .allowCredentials(true) //자격증명 허용
+                .maxAge(MAX_AGE_SECS);
+    }
 }
