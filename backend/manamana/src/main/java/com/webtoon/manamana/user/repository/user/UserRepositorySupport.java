8c18565ef419026882ad75e93095610ac0a70e00
lsh80165 <lsh80165@gmail.com>
2023-03-29 14:21:46 +0900

    Fix : 쿼리문 수정 테스트

--- backend/manamana/src/main/java/com/webtoon/manamana/user/repository/user/UserRepositorySupport.java
+++ backend/manamana/src/main/java/com/webtoon/manamana/user/repository/user/UserRepositorySupport.java
@@ -1,6 +1,8 @@
 package com.webtoon.manamana.user.repository.user;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.webtoon.manamana.entity.user.LoginProvider;
+import com.webtoon.manamana.entity.user.QLoginProvider;
 import com.webtoon.manamana.entity.user.QUser;
 import com.webtoon.manamana.entity.user.User;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +10,7 @@ import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class UserRepositorySupport extends QuerydslRepositorySupport {
@@ -17,6 +20,20 @@ public class UserRepositorySupport extends QuerydslRepositorySupport {
     public UserRepositorySupport(JPAQueryFactory queryFactory) {
         super(User.class);
         this.queryFactory = queryFactory;
+    }
+
+    //유저와 provider 같이 조회
+    public Optional<User> findUserByEmailAndLoginProvider(String email, LoginProvider loginProvider){
+
+        QUser user = QUser.user;
+        return Optional.ofNullable(
+                queryFactory
+                        .selectFrom(user)
+                        .leftJoin(user.loginProvider, QLoginProvider.loginProvider)
+                        .fetchJoin()
+                        .where(user.email.eq(email), user.loginProvider.eq(loginProvider))
+                        .fetchOne()
+        );
     }
 
 //    public List<User> findByNickname(String nickname){
