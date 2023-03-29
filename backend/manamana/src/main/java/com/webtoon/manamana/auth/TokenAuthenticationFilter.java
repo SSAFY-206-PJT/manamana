eff267bc1e76ee75b588ca3ae89fea59c2e93628
lsh80165 <lsh80165@gmail.com>
2023-03-29 14:44:12 +0900

    Fix : 긴급 수정

--- backend/manamana/src/main/java/com/webtoon/manamana/auth/TokenAuthenticationFilter.java
+++ backend/manamana/src/main/java/com/webtoon/manamana/auth/TokenAuthenticationFilter.java
@@ -1,17 +1,17 @@
 package com.webtoon.manamana.auth;
 
-import com.webtoon.manamana.auth.util.TokenProvider;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+//import com.webtoon.manamana.auth.util.TokenProvider;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.filter.OncePerRequestFilter;
+//
+//import javax.servlet.FilterChain;
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
 //@Slf4j
 //@RequiredArgsConstructor
 //@Component
