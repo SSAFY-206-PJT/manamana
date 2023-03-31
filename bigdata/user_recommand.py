48c02cb484edf83924a5d1ca02a4ae710f7ce263
oth54477 <oth5447@naver.com>
2023-03-31 13:42:10 +0900

    Feat : recommand

---
+++ bigdata/user_recommand.py
@@ -0,0 +1,88 @@
+from sklearn.decomposition import TruncatedSVD
+from scipy.sparse.linalg import svds
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import warnings
+import json
+import requests
+warnings.filterwarnings("ignore")
+
+def recommand_movies(df_svd_preds, user_id, ori_webtoons_df, ori_scores_df, num_recommandations=5):
+    
+    #현재는 index로 적용이 되어있으므로 user_id - 1을 해야함.
+    user_row_number = user_id - 1 
+    
+    # 최종적으로 만든 pred_df에서 사용자 index에 따라 영화 데이터 정렬 -> 영화 평점이 높은 순으로 정렬 됌
+    sorted_user_predictions = df_svd_preds.iloc[user_row_number].sort_values(ascending=False)
+    
+    # 원본 평점 데이터에서 user id에 해당하는 데이터를 뽑아낸다. 
+    user_data = ori_scores_df[ori_scores_df.userId == user_id]
+    
+    # 위에서 뽑은 user_data와 원본 영화 데이터를 합친다. 
+    # user_history = user_data.merge(ori_webtoons_df, on = 'webtoonId').sort_values(['score'], ascending=False)
+    user_history = user_data.merge(ori_webtoons_df, on = 'webtoonId')
+    # user_history = user_data
+    
+    # 원본 영화 데이터에서 사용자가 본 영화 데이터를 제외한 데이터를 추출
+    recommandations = ori_webtoons_df[~ori_webtoons_df['webtoonId'].isin(user_history['webtoonId'])]
+    # 사용자의 영화 평점이 높은 순으로 정렬된 데이터와 위 recommandations을 합친다. 
+    recommandations = recommandations.merge( pd.DataFrame(sorted_user_predictions).reset_index(), on = 'webtoonId')
+    # 컬럼 이름 바꾸고 정렬해서 return
+    recommandations = recommandations.rename(columns = {user_row_number: 'Predictions'}).sort_values('Predictions', ascending = False).iloc[:num_recommandations, :]
+
+
+    return user_history, recommandations
+
+
+
+
+def recommand_to_user(data, user_id):
+    """
+    사용자 기반 추천
+        ARGS:
+            data: list
+        RETURN:
+            recommand: DataFrame
+    """
+    df = pd.DataFrame(data)
+    temp = []
+    for i in range(100):
+        temp_dict = {"webtoonId": i}
+        temp.append(temp_dict)
+    df_ori = pd.DataFrame(temp)
+    
+    df_ori.drop_duplicates(subset=None, keep='first', inplace=False, ignore_index=False)
+    print(df_ori)
+    df_user_webtoon_scores = df.pivot(
+        index='userId',
+        columns='webtoonId',
+        values='score'
+    ).fillna(0)
+
+    # matrix는 pivot_table 값을 numpy matrix로 만든 것 
+    matrix = df_user_webtoon_scores.values
+
+    # user_ratings_mean은 사용자의 평균 평점 
+    user_scores_mean = np.mean(matrix, axis = 1)
+
+    # R_user_mean : 사용자-영화에 대해 사용자 평균 평점을 뺀 것.
+    matrix_user_mean = matrix - user_scores_mean.reshape(-1, 1)
+
+    # scipy에서 제공해주는 svd.  
+    # U 행렬, sigma 행렬, V 전치 행렬을 반환.
+
+    U, sigma, Vt = svds(matrix_user_mean, k = 12)
+
+    sigma = np.diag(sigma)
+
+    # U, Sigma, Vt의 내적을 수행하면, 다시 원본 행렬로 복원이 된다. 
+    # 거기에 + 사용자 평균 rating을 적용한다. 
+    svd_user_predicted_scores = np.dot(np.dot(U, sigma), Vt) + user_scores_mean.reshape(-1, 1)
+
+    df_svd_preds = pd.DataFrame(svd_user_predicted_scores, columns = df_user_webtoon_scores.columns)
+
+    already_rated, predictions = recommand_movies(df_svd_preds, user_id, df_ori, df, 10)
+    return predictions['webtoonId'].values.tolist()
--- bigdata/webtoonRecommand.py
+++ bigdata/webtoonRecommand.py
@@ -4,22 +4,19 @@ import pandas as pd
 import numpy as np
 
 
-
-
 # 웹툰 추천
 def recommand_webtoon(data: list):
     """
     웹툰 추천
         ARGS:
             data: list
+            
     """
-
-    
     webtoon_list = data # 웹툰 데이터
     L = len(webtoon_list)
     df = pd.DataFrame(webtoon_list)
 
-    user_webtoon_score = df.pivot_table('score', index = 'userId', columns='webtoonId').fillna(0) # vaule: score, column: webtoonId, row: userId
+    user_webtoon_score = df.pivot_table('score', index = 'userId', columns='webtoonId').fillna(0) # vaule: score, column: webtoon, row: user
 
     
     SVD = TruncatedSVD(n_components=12) # 특이값 분해(latent: 12)
@@ -33,23 +30,21 @@ def recommand_webtoon(data: list):
     corr = np.corrcoef(matrix) # 피어슨 상관계수 구하기
 
     webtoon_title = user_webtoon_score.columns
-    
+
+    np.save('corr.npy', corr)
+    f = open("column_list.txt", 'w', encoding="UTF-8")
+    f.write(' '.join(list(map(str, list(webtoon_title)))))
+
     return webtoon_title, corr
 
-def webtoons_recommand_top10(webtoon_title, corr, target_webtoon_id: int) -> list:
-    """
-    상위 10개 추출
-        ARGS:
-            webtoon_title
-            corr
-            target_webtoon_id: int
-    """
-    webtoon_title_list = list(webtoon_title)
+def webtoons_recommand_top10(target_webtoon_id: int) -> list:
+    f = open("column_list.txt", 'r', encoding="UTF-8")
+    webtoon_title_list = list(map(int, f.read().split()))
+    corr = np.load('corr.npy')
+
     coffey_hands = webtoon_title_list.index(target_webtoon_id)
     corr_coffey_hands = corr[coffey_hands]
 
-    sort_by_corr = sorted(list(zip(webtoon_title, corr_coffey_hands)), key=lambda x: x[1], reverse=True)
+    sort_by_corr = sorted(list(zip(webtoon_title_list, corr_coffey_hands)), key=lambda x: x[1], reverse=True)
     recommand_webtoon = list(map(lambda x: x[0], sort_by_corr[1:]))
-    print(recommand_webtoon)
     return recommand_webtoon[:10] # 가장 추천하는 10개만 리턴
-
