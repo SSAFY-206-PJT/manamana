0381e7f86b6b156af58f3df04eb86fb21e6044c9
happyd918 <0happydeath0@naver.com>
2023-04-03 14:14:39 +0900

    Feat : 감상하러가기, mui 아이콘

--- frontend/components/pages/detail/GoSee.tsx
+++ frontend/components/pages/detail/GoSee.tsx
@@ -1,32 +1,42 @@
+import { getWebtoonProviders } from '@/pages/api/detail';
+import { getCookie } from '@/util/cookie';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 interface Props {
-  webtoonProvider: string;
+  webtoonId: number;
 }
 
-function GoSee({ webtoonProvider }: Props) {
+function GoSee({ webtoonId }: Props) {
+  const token = getCookie('accessToken');
+  const [provider, setProvider] = useState<any>(null);
+
+  const getProviders = async () => {
+    const res = await getWebtoonProviders(webtoonId, token);
+    console.log(res);
+    if (res.result) {
+      setProvider(res.result);
+    }
+  };
+
+  useEffect(() => {
+    getProviders();
+  }, []);
+
+  if (!provider) {
+    return <div>로딩중</div>;
+  }
   return (
     <div className="absolute bottom-1/2 right-1/2 translate-x-1/2 translate-y-1/2 rounded bg-white p-4">
       <p className="mb-2 whitespace-nowrap">클릭시 해당 플랫폼으로 이동 합니다</p>
       <div className="flex justify-evenly">
         <div className="inline-block">
-          <Link href={webtoonProvider} target="_blank">
-            <img className="h-12 w-12" src="/images/Naver_Webtoon_Logo.png"></img>
-            <button className="text-sm font-bold">보러가기</button>
+          <Link href={provider.url} target="_blank">
+            <img className="mx-auto h-12 w-12" src={provider.provider_image}></img>
+            <div className="mx-auto text-sm font-bold">{provider.name}</div>
+            <div className="mx-auto w-fit text-sm font-bold">보러가기</div>
           </Link>
         </div>
-        {/* <div className="inline-block">
-          <Link href={webtoonProvider} target="_blank">
-            <img className="h-12 w-12" src="/images/Kakao_Webtoon_Logo.png"></img>
-            <button className="text-sm font-bold">보러가기</button>
-          </Link>
-        </div>
-        <div className="inline-block">
-          <Link href={webtoonProvider} target="_blank">
-            <img className="h-12 w-12" src="/images/Kakao_Page_Logo.png"></img>
-            <button className="text-sm font-bold">보러가기</button>
-          </Link>
-        </div> */}
       </div>
     </div>
   );
