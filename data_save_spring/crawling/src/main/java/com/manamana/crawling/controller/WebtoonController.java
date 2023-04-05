94334449b61149e9334df51b9e0baa7a7e7620bf
oth54477 <oth5447@naver.com>
2023-04-05 14:20:24 +0900

    Fix : 테스트 코드 제거

--- data_save_spring/crawling/src/main/java/com/manamana/crawling/controller/WebtoonController.java
+++ data_save_spring/crawling/src/main/java/com/manamana/crawling/controller/WebtoonController.java
@@ -1,13 +1,9 @@
 package com.manamana.crawling.controller;
 
-import com.manamana.crawling.dto.ResponseRecommendDataDTO;
 import com.manamana.crawling.dto.WebtoonDataArrayDTO;
-import com.manamana.crawling.dto.WebtoonUpdateDTO;
-import com.manamana.crawling.service.RecommendService;
 import com.manamana.crawling.service.WebtoonService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,16 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class WebtoonController {
 
     private final WebtoonService webtoonService;
-    private final RecommendService recommendService;
 
     @PostMapping("/crawling")
     public Long saveWebtoon(@RequestBody WebtoonDataArrayDTO webtoonDataArrayDTO) {
         webtoonService.webtoonsData(webtoonDataArrayDTO);
         return 1L;
-    }
-
-    @GetMapping("/recommend")
-    public ResponseRecommendDataDTO recommend() {
-        return recommendService.dummyRecommendData();
     }
 }
