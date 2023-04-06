3efc4d357a29f44d1314486d7c2a29c5af8f6db6
lsh80165 <lsh80165@gmail.com>
2023-04-07 08:47:16 +0900

    Feat : 도커파일 추가 - 타임존 변경

--- backend/manamana/src/main/java/com/webtoon/manamana/ManamanaApplication.java
+++ backend/manamana/src/main/java/com/webtoon/manamana/ManamanaApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class ManamanaApplication {
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(ManamanaApplication.class, args);
