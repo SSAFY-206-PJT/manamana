886360575c7cab1aafeabf9a0575f2241d8161fb
dlaqjarb9 <dlaqjarb9@daum.net>
2023-04-06 14:14:43 +0900

    Feat : 알림 페이지 구현 완료

--- frontend/components/pages/notification/notiItem.tsx
+++ frontend/components/pages/notification/notiItem.tsx
@@ -1,20 +1,31 @@
-type Props = {
-  imageUrl: string;
-  webtoonName: string;
-  lastTurn: number;
-};
-
+interface Props {
+  key: number;
+  image: any;
+  name: string;
+  episode: number;
+  webtoonId: number;
+}
+import { useRouter } from 'next/router';
 export default function notiItem(props: Props) {
+  const router = useRouter();
+  console.log('props', props);
   return (
     <div className="mx-4 my-4 flex flex-row justify-between">
       <div className="ml-3 flex">
-        <img src={props.imageUrl} alt="" className="h-18 w-12"></img>
+        <img src={props.image} alt="" className="h-18 w-12"></img>
         <div className="ml-3 flex flex-col">
-          <div className="font-bold">{props.webtoonName}</div>
-          <div>{props.lastTurn}회가 등록되었습니다.</div>
+          <div className="font-bold">{props.name}</div>
+          <div>{props.episode}회가 등록되었습니다.</div>
         </div>
       </div>
-      <div className="flex items-center justify-end font-bold">보러가기</div>
+      <div
+        className="flex items-center justify-end font-bold"
+        onClick={() => {
+          router.push(`/detail/${props.webtoonId}`);
+        }}
+      >
+        보러가기
+      </div>
     </div>
   );
 }
